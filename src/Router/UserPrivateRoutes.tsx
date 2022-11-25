6d1ac00e1466133869da9f897a8b0bd7836262e5
hito-kotaro <tohi.work@gmail.com>
2022-11-25 13:50:20 +0900

    Route分岐修正

--- src/Router/UserPrivateRoutes.tsx
+++ src/Router/UserPrivateRoutes.tsx
@@ -2,37 +2,27 @@ import React, { useEffect } from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 import useAdminState from '../recoil/adminState/useAdminState'
 import useAuthState from '../recoil/authState/useAuthState'
-import useLogin, { authCheck } from '../Repositories/auth/useLogin'
+import useLogin, { authCheckType } from '../Repositories/auth/useLogin'
 
 const UserPrivateRoutes = () => {
   const { isAuth, setIsAuth } = useAuthState()
   const { setIsAdmin } = useAdminState()
-  const token = localStorage.getItem('token')
   const { validate } = useLogin()
 
   useEffect(() => {
-    if (token) {
-      validate().then((res: authCheck) => {
-        setIsAuth(res.auth)
-        setIsAdmin(res.admin)
-      })
-    }
+    validate().then((res: authCheckType) => {
+      setIsAuth(res.auth)
+      setIsAdmin(res.admin)
+    })
   }, [])
 
-  const switchRoute = () => {
-    // isAuthがnullの場合何もしない
-    if (!isAuth) {
-      return null
-    }
-
-    // isAuthがbooleanの場合t/fによってreturn
-    if (isAuth === true) {
-      return <Outlet />
-    }
-    return <Navigate to="/login/user" />
+  // nullなら先にreturn
+  if (isAuth === null) {
+    return null
   }
 
-  return switchRoute()
+  // true/false
+  return isAuth ? <Outlet /> : <Navigate to="/login/user" />
 }
 
 export default UserPrivateRoutes
